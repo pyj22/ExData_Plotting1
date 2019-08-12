a6808dc5f98cb520d7ac4cfa736d0c67d3158ef8
Pierre Leroy <pleroy38@gmail.com>
2019-08-12 22:19:34 +0200

    Assignment Commit

---
+++ Plot2.R
@@ -0,0 +1,10 @@
+rawdata <- read.csv("household_power_consumption.txt", header = T, sep = ';', 
+                      na.strings = "?", nrows = 2075259, check.names = F, 
+                      stringsAsFactors = F, comment.char = "", quote = '\"')
+rawdata$Date <- as.Date(rawdata$Date, format = "%d/%m/%Y")
+data <- subset(rawdata, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(rawdata)
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+plot(data$Global_active_power ~ data$Datetime, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
